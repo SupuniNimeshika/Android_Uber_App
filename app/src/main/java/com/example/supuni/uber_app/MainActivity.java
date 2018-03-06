a047255817b60086d7f353fe1ffc0e11692ec19b
Your Name <supuni.nimeshika@gmail.com>
2018-03-06 22:52:23 +0530

    Connect Two Activities To Main Activity

--- app/src/main/java/com/example/supuni/uber_app/MainActivity.java
+++ app/src/main/java/com/example/supuni/uber_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.supuni.uber_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void open_signUp(View v) {
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
+    }
+
+    public void open_signIn(View v) {
+        Intent i = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(i);
     }
 }
