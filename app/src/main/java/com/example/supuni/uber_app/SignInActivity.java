f02210de352c134ead82abb6ffab46d201d7da23
Your Name <supuni.nimeshika@gmail.com>
2018-03-07 17:22:45 +0530

    Add Dialog When Signing

--- app/src/main/java/com/example/supuni/uber_app/SignInActivity.java
+++ app/src/main/java/com/example/supuni/uber_app/SignInActivity.java
@@ -1,17 +1,35 @@
 package com.example.supuni.uber_app;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
     EditText email,password;
+    FirebaseAuth auth;
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         email =findViewById(R.id.editEmail);
         password =findViewById(R.id.editPassword);
+
+//        get Firebase Instance
+        auth =FirebaseAuth.getInstance();
+        dialog =new ProgressDialog(this);
+    }
+
+    public void signinUser(View v){
+        dialog.setMessage("Signing in. Please wait");
+        dialog.show();
+
+        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString());
     }
 }
