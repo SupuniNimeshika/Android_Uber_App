741c836ad74b2f745b6d655240d1121d6d40f665
Your Name <supuni.nimeshika@gmail.com>
2018-03-07 17:31:27 +0530

    Add Functionality To Signin

--- app/src/main/java/com/example/supuni/uber_app/SignInActivity.java
+++ app/src/main/java/com/example/supuni/uber_app/SignInActivity.java
@@ -1,11 +1,16 @@
 package com.example.supuni.uber_app;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
@@ -30,6 +35,23 @@ public class SignInActivity extends AppCompatActivity {
         dialog.setMessage("Signing in. Please wait");
         dialog.show();
 
-        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString());
+        if(email.getText().toString().equals("") || password.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Fields cannot be empty",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"User successfully signed in",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"User not found",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
     }
 }
